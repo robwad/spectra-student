ed21aa45d6ae65aa3d750da72a57fd4265fdf206
Robert Waddilove <darleywaddilove@college.harvard.edu>
2018-04-01 19:55:14 -0400

    d

---
+++ js/teacher.js
@@ -0,0 +1,19 @@
+<div class="flex-grid">
+  <div class="col">Stuart</div>
+  <div class="col">Tom</div>
+  <div class="col">James</div>
+</div>
+
+<div class="flex-grid">
+  <div class="col">Henry</div>
+  <div class="col">Alice</div>
+  <div class="col">Jen</div>
+  <div class="col">Matt</div>
+  <div class="col">Julia</div>
+</div>
+
+<div class="flex-grid-thirds">
+  <div class="col">Christine.</div>
+  <div class="col">Jose</div>
+  <div class="col">Kevin</div>
+</div>
