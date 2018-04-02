68b97c3c12f5c863002e376ad487491dee568c99
Robert Waddilove <darleywaddilove@college.harvard.edu>
2018-04-01 20:34:15 -0400

    D

--- js/teacher.js
+++ js/teacher.js
@@ -1,19 +1,37 @@
-<div class="flex-grid">
-  <div class="col">Stuart</div>
-  <div class="col">Tom</div>
-  <div class="col">James</div>
-</div>
+// .flex-grid {
+//   display: flex;
+// }
+// .flex-grid .col {
+//   flex: 1;
+// }
+// .flex-grid-thirds {
+//   display: flex;
+//   justify-content: space-between;
+// }
+// .flex-grid-thirds .col {
+//   width: 32%;
+// }
 
-<div class="flex-grid">
-  <div class="col">Henry</div>
-  <div class="col">Alice</div>
-  <div class="col">Jen</div>
-  <div class="col">Matt</div>
-  <div class="col">Julia</div>
-</div>
-
-<div class="flex-grid-thirds">
-  <div class="col">Christine.</div>
-  <div class="col">Jose</div>
-  <div class="col">Kevin</div>
-</div>
+// @media (max-width: 400px) {
+//   .flex-grid,
+//   .flex-grid-thirds {
+//     display: block;
+//     .col {
+//       width: 100%;
+//       margin: 0 0 10px 0;
+//     }
+//   }
+// }
+// // * {
+// //   box-sizing: border-box;
+// // }
+// // body {
+// //   padding: 20px;
+// // }
+// // .flex-grid { 
+// //   margin: 0 0 20px 0;
+// // }
+// // .col {
+// //   background: salmon;
+// //   padding: 20px;
+// // }
