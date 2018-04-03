ac3d89aff2c363191b61f0bf5af0e0f46c30b6df
Robert Waddilove <darleywaddilove@college.harvard.edu>
2018-04-03 14:56:37 -0400

    d

--- js/teacher.js
+++ js/teacher.js
@@ -1,37 +1,3 @@
-// .flex-grid {
-//   display: flex;
-// }
-// .flex-grid .col {
-//   flex: 1;
-// }
-// .flex-grid-thirds {
-//   display: flex;
-//   justify-content: space-between;
-// }
-// .flex-grid-thirds .col {
-//   width: 32%;
-// }
 
-// @media (max-width: 400px) {
-//   .flex-grid,
-//   .flex-grid-thirds {
-//     display: block;
-//     .col {
-//       width: 100%;
-//       margin: 0 0 10px 0;
-//     }
-//   }
-// }
-// // * {
-// //   box-sizing: border-box;
-// // }
-// // body {
-// //   padding: 20px;
-// // }
-// // .flex-grid { 
-// //   margin: 0 0 20px 0;
-// // }
-// // .col {
-// //   background: salmon;
-// //   padding: 20px;
-// // }
+  var elem = document.querySelector('.sidenav');
+  var instance = M.Sidenav.init(elem, {});
