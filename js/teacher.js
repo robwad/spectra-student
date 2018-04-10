d84668ebc028bb7b57b0232e503114068a814e9f
Robert Waddilove <darleywaddilove@college.harvard.edu>
2018-04-10 09:59:18 -0400

    working

--- js/teacher.js
+++ js/teacher.js
@@ -1,3 +1,16 @@
 
   var elem = document.querySelector('.sidenav');
   var instance = M.Sidenav.init(elem, {});
+
+let students = [
+  {
+    name: null,
+    comment: null,
+    alert: false,
+    visible: false
+  }
+]
+
+function renderStudent(stud) {
+  return '<div '
+}
