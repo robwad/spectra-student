fcb8d92e7c7e0bf7ce3c130d5325f27e3d2eec8c
Robert Waddilove <darleywaddilove@college.harvard.edu>
2018-04-11 19:28:54 -0400

    D

--- js/teacher.js
+++ js/teacher.js
@@ -191,7 +191,7 @@ function editJen() {
   students[12].name = currentJen.name;
   students[12].comment = currentJen.comment;
   students[12].alert = currentJen.alert;
-  students[12].mood[1] = currentJen.mood;
+  // students[12].mood[1] = currentJen.mood;
 }
 editJen();
 renderClass(students);
